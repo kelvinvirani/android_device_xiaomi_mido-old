03cf05e532df4a2daaef983427518c91b7b23053
xyyx <xyyx@mail.ru>
2017-09-23 18:05:00 +0800

    Add missed mediaextractor.policy

--- device.mk
+++ device.mk
@@ -300,7 +300,8 @@ PRODUCT_COPY_FILES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
 
 # USB HAL
 PRODUCT_PACKAGES += \
